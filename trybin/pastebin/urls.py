781f29acb35427d428a14d2e625e54ca47a30c0e
SAMFATEWOLF <s.v.bolonin@mail.ru>
2024-01-04 03:32:08 +0300

    new try 2

--- trybin/pastebin/urls.py
+++ trybin/pastebin/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
-
 ]
